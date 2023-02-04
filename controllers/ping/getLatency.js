212c8981c35d49bdc04bd0079f32d42970effb78
aleksandrchuyko <chao.mess@gmail.com>
2023-02-04 20:04:38 +0200

    Update getLatency.js

--- controllers/ping/getLatency.js
+++ controllers/ping/getLatency.js
@@ -7,6 +7,7 @@ const getLatency = async (req, res) => {
 	const lat = await ping.promise.probe("www.google.com", {
 		timeout: false,
 		// extra: ['-i', '2'],
+		v6: true
 });
 res.status(200).json({ latency: lat });
 }
