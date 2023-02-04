c1cb38475f1dd8ed87f943457336d24348020b43
aleksandrchuyko <chao.mess@gmail.com>
2023-02-04 21:05:28 +0200

    Update getLatency.js

--- controllers/ping/getLatency.js
+++ controllers/ping/getLatency.js
@@ -4,13 +4,21 @@ const ping = require('ping');
 
 
 const getLatency = async (req, res) => {
-	const lat = await ping.promise.probe('google.com', {
-		timeout: false,
-		extra: ['-i', '2'],
+// 	const lat = await ping.promise.probe('google.com', {
+// 		timeout: false,
+// 		extra: ['-i', '2'],
 
+// });
+
+// res.status(200).json({ latency: lat });
+var hosts = ['192.168.1.1', 'google.com', 'yahoo.com'];
+hosts.forEach(function(host){
+    ping.sys.probe(host, function(isAlive){
+        const msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+        console.log(msg);
+    });
 });
-
-res.status(200).json({ latency: lat });
+res.status(200).json({ latency: 'lat' });
 }
 
 
